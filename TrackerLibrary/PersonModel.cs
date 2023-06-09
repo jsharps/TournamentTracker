c1e8e235d97b99c5f256972f1b3a50b71c531675
Jefferson S Manrique <adminextjmanrique@gn.com>
2023-06-09 15:44:10 +0800

    initialize class project

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackerLibrary
+{
+	public class PersonModel
+	{
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string EmailAddress { get; set; } = string.Empty;
+	}
+}
---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerLibrary
+{
+	public class PrizeModel
+	{
+        public int PlaceNumber { get; set; }
+		public string PlaceName { get; set; } = string.Empty;
+		public decimal PrizeAmount { get; set; }
+		public decimal PricePercentage { get; set; }
+	}
+}

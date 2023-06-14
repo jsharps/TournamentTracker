c79c0978f77a9d34a7894d3fc9978516e284a3f6
Jefferson S Manrique <adminextjmanrique@gn.com>
2023-06-14 20:14:22 +0800

    db changes

--- TournamentsDB/dbo/Players.sql
+++ TournamentsDB/dbo/Players.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[Players]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [EmailAdress] NVARCHAR(50) NULL, 
+    [CellphoneNumber] NVARCHAR(50) NULL 
 )
--- TournamentsDB/dbo/TournamentPrizes.sql
+++ TournamentsDB/dbo/TournamentPrizes.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[TournamentPrizes]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL, 
+    [TournamentId] INT NULL, 
+    [PrizeId] INT NULL, 
+    CONSTRAINT [PK_TournamentPrizes] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_TournamentPrizes_ToTournaments] FOREIGN KEY (TournamentId) REFERENCES dbo.[Tournaments](Id
+    ), 
+    CONSTRAINT [FK_TournamentPrizes_ToPrizes] FOREIGN KEY (PrizeId) REFERENCES dbo.[Prizes]([Id]) 
 )
